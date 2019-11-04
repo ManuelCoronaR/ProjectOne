b48e8216c75a6d735550e6a240320abc6a84adbd
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2019-11-04 17:10:45 -0500

    add functions that lead to each of the webpage (dentists,oculists, physicians, and veterinarians, and add the function for re-start all in the app.js files, also add button to the re-start in the index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 // jQuery Tool button from Materialize.com
 
-$('.fixed-action-btn').floatingActionButton({
+/*$('.fixed-action-btn').floatingActionButton({
     toolbarEnabled: true
-});
+});*/
 
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
 });
@@ -14,4 +14,39 @@ $(".material-icons").css("color".black);
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, options);
+});*/
+
+//Function  to re-start the app. The id has been named "str1"
+
+$('#str1').on('click', function rStart() {
+    window.location.reload(false);
+});
+
+
+//Function that takes to the Dentists html page.
+
+$('#btn1').on('click', function Dentist() {
+    window.location.href = "/cards.html";
+
+});
+
+//Function that takes to the Oculists html page.
+
+$('#btn2').on('click', function Oculist() {
+    window.location.href = "/cards.html";
+
+});
+
+//Function that takes to the Physicians html page.
+
+$('#btn3').on('click', function Physician() {
+    window.location.href = "/cards.html";
+
+});
+
+//Function that takes to the Veterinarians html page.
+
+$('#btn4').on('click', function Veterinarian() {
+    window.location.href = "/cards.html";
+
 });
